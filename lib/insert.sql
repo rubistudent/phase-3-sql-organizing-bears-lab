56a9c0856c11e8850cddf037c66fed73f23d6f8a
www.rubiluqman7@gmail.com <www.rubiluqman7@gmail.com>
2023-02-19 15:33:15 +0300

    completed

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 23, "M", "uses python", "fast", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 88, "M", "uses C#", "very old", 0);
+INSERT INTO bears VALUES (3,"Tabitha", 96, "F", "uses html", "naive", 1)
+INSERT INTO bears VALUES (4,"Sargeant Brown", 1, "M", "uses VB", "stubborn", 1);
+INSERT INTO bears VALUES (5,"Melissa", 14, "F", "uses js", "mad man", 1);
+INSERT INTO bears VALUES (6,"Grinch", 99, "M", "uses swift", "ganned", 1);
+INSERT INTO bears VALUES (7,"Wendy", 63, "F", "uses macbook", "not a dj", 0);
+INSERT INTO bears VALUES (8,null, 69, "M", "nulled", "confussion", 0);
