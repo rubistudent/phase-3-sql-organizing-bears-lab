b1586c6e99bdd97d430b9719c012924412f22baa
www.rubiluqman7@gmail.com <www.rubiluqman7@gmail.com>
2023-03-08 19:27:23 +0300

    re done

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears VALUES (1,"Mr. Chocolate", 23, "M", "uses python", "fast", 1);
-INSERT INTO bears VALUES (2,"Rowdy", 88, "M", "uses C#", "very old", 0);
-INSERT INTO bears VALUES (3,"Tabitha", 96, "F", "uses html", "naive", 1)
-INSERT INTO bears VALUES (4,"Sargeant Brown", 1, "M", "uses VB", "stubborn", 1);
-INSERT INTO bears VALUES (5,"Melissa", 14, "F", "uses js", "mad man", 1);
-INSERT INTO bears VALUES (6,"Grinch", 99, "M", "uses swift", "ganned", 1);
-INSERT INTO bears VALUES (7,"Wendy", 63, "F", "uses macbook", "not a dj", 0);
-INSERT INTO bears VALUES (8,null, 69, "M", "nulled", "confussion", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Mr. Chocolate", 18, "M", "Brown", "moderate", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Rowdy", 18, "M", "Brown", "moderate", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Tabitha", 18, "F", "Brown", "moderate", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Sergeant Brown", 18, "M", "Brown", "moderate", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Melissa", 18, "F", "Brown", "moderate", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Grinch", 18, "M", "Brown", "moderate", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Wendy", 18, "F", "Brown", "moderate", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES(NULL, 18, "M", "Brown", "moderate", TRUE);   
